dff8fb2dd58d6aa800bb050fbed1bf0e7c86e9d3
Andrea Cancellieri <116791211+Keinssz@users.noreply.github.com>
2023-05-16 16:44:54 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace cancellieri.andrea._4h.GUIDb;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace cancellieri.andrea._4h.GUIDb;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
